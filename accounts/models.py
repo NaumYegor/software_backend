030d5d8b6056ffc4241eab69a7427ba4458d9ca9
naum <yehornaumenko@gmail.com>
2019-12-15 19:26:34 +0200

    Refactored

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Client(models.Model):
-    name = models.CharField(max_length = 30)
-    surname = models.CharField(max_length = 30)
-    email = models.EmailField(max_length = 100)
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
 
-    status = models.CharField(max_length = 30, choices = [('a', 'Admin'), ('u', 'User')])
-    account = models.ForeignKey(User, on_delete = models.CASCADE, null = True)
+    status = models.CharField(max_length=30, choices=[('a', 'Admin'), ('u', 'User')])
+    account = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return self.name + ' ' + self.surname
+        return '{} {}'.format(self.name, self.surname)
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import sign_in, logout, registration, restore_password
 
 urlpatterns = [
-    path('sign_in/', sign_in, name = 'sign_in'),
-    path('logout/', logout, name = 'logout'),
-    path('registration/', registration, name = 'registration'),
-    path('restore_password/', restore_password, name = 'restore_password')
+    path('sign_in/', sign_in, name='sign_in'),
+    path('logout/', logout, name='logout'),
+    path('registration/', registration, name='registration'),
+    path('restore_password/', restore_password, name= 'restore_password')
 ]
