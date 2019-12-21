a5c129aa456992db4a0aeb14fff054aa78eafab7
Omelman <54813861+Omelman@users.noreply.github.com>
2019-12-22 01:32:03 +0200

    added new routes

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import home, edu_info, company_info
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('edu_plan/', edu_info),
+    path('company_info/', company_info)
 ]
