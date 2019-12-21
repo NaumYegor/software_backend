78f44c575a94fad121b2b4b0a68f81e8355c81bd
naum <yehornaumenko@gmail.com>
2019-12-21 20:13:10 +0200

    Updates photos according to specified period

--- insta/views.py
+++ insta/views.py
@@ -1,18 +1,29 @@
 from django.shortcuts import render, HttpResponse
 import instagram
 import json
+import datetime as dt
 
 account_name = 'student.nure'
+update_period = 300
+links = list()
+last_stamp = dt.datetime.now().timestamp()-update_period
 
 
 def recent_photos(request):
 
     if request.method == 'GET':
+        global links
+        global last_stamp
+
+        if dt.datetime.now().timestamp()-last_stamp < update_period:
+            return HttpResponse(json.dumps(links))
+        else:
+            links = []
+
         amount = request.GET["amount"]
         if not amount.isdigit():
             return HttpResponse("Amount's been entered in a wrong way.")
         else:
-            links = []
             amount = int(amount)
             agent = instagram.WebAgent()
             account = instagram.Account(account_name)
@@ -20,6 +31,7 @@ def recent_photos(request):
             for content in media:
                 link = 'https://www.instagram.com/p/{}/media/'.format(content)
                 links.append(link)
+            last_stamp = dt.datetime.now().timestamp()
             return HttpResponse(json.dumps(links))
     else:
         return HttpResponse("This method is not allowed.")
